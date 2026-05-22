115e1a5eeb463b2a0f30dd23b4f89b568cf7c21d
najibra <najib.ramadan@GMAIL.COM>
2026-05-22 21:14:10 +0400

    Bust cached Kraken deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister().catch(() => undefined);
+    });
+  }).catch(() => undefined);
+}
+
+if ("caches" in window) {
+  caches.keys().then((keys) => {
+    keys.forEach((key) => {
+      caches.delete(key).catch(() => undefined);
+    });
+  }).catch(() => undefined);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
